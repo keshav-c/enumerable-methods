f192298eb32e257b0a0070e1581ea71531fa5a2a
keshav-c <keshav1985@gmail.com>
2019-06-11 11:02:56 +0530

    Add and test my_all?

--- main.rb
+++ main.rb
@@ -18,6 +18,11 @@ module Enumerable
         filtered
     end
 
+    def my_all?
+        self.my_each {|item| return false unless yield(item)}
+        true
+    end
+
 end
 
 ar = ["hello", 42, "an array", :sym, true, :sym2, 3.14]
@@ -32,5 +37,12 @@ ar = ["hello", 42, "an array", :sym, true, :sym2, 3.14]
 
 # my_select test
 # ------------------
-puts ar.my_select {|item| item.is_a? Numeric}.join(", ")
-puts ar.my_select {|item| item.is_a? Symbol}.join(", ")
+# puts ar.my_select {|item| item.is_a? Numeric}.join(", ")
+# puts ar.my_select {|item| item.is_a? Symbol}.join(", ")
+
+
+# my_all? test
+# ------------------
+puts ar.my_all? {|item| item.is_a? String}
+ar2 = [1, 2, 3, 4, 5]
+puts ar2.my_all? {|item| item.is_a? Integer}
