1fd0bab706bdf3782ba4517db6a46329cb0bc185
keshav-c <keshav1985@gmail.com>
2019-06-11 10:41:46 +0530

    Add and test my_each_with_index

--- main.rb
+++ main.rb
@@ -6,9 +6,18 @@ module Enumerable
         end
     end
 
+    def my_each_with_index
+        for idx in (0...self.size)
+            yield(self[idx], idx)
+        end
+    end
+
 end
 
+ar = ["hello", 42, :sym, true]
 
 # my_each test
-ar = ["hello", 42, :sym, true]
-ar.my_each {|item| puts item}
+ar.my_each {|item| puts "#{item}"}
+
+# my_each_with_index test
+ar.my_each_with_index {|item, i| puts "#{item} #{i}"}
